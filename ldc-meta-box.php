a2884dd7d7e30ed0367aa8b38d3e8408da520f23
Luis del Cid <luisdelcid@Luiss-MacBook.local>
2020-03-13 12:06:13 -0600

    Test

--- ldc-meta-box.php
+++ ldc-meta-box.php
@@ -16,7 +16,7 @@
 	defined('ABSPATH') or die('No script kiddies please!');
 		add_action('ldc_plugin_loaded', function(){
 		if(!class_exists('LDC_Meta_Box', false)){
-			require_once(plugin_dir_path(__FILE__) . 'class-ldc-meta-box.php');
+			require_once(plugin_dir_path(__FILE__) . 'classes/class-ldc-meta-box.php');
 			LDC_Meta_Box::init(__FILE__, '0.3.10');
 		}
 	});
